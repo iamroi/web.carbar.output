7c6d74f2889c51ae0da43b32b01faff87b71ec48
Raja Munisamy <raja.munisamy@carbar.com.au>
2022-02-23 17:04:37 +1100

    'wip'

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
-copy C:\Workspace\web.carbar\.output C:\Workspace\output-web-carbar\.output
+xcopy /E/H C:\Workspace\web.carbar\.output C:\Workspace\output-web-carbar\.output
 git add .
 git commit -m 'wip'
 git push -f -u origin master
